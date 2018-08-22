b13f1899d9e51b2358390f10ac5076022ef3791f
wjcooper91 <wjcooper91@gmail.com>
2018-08-21 21:43:55 -0400

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Pull in required dependencies
 var mysql = require('mysql');
 
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 // Create the MySQL connection object
 var connection = mysql.createConnection({
-		port: 8889,
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'tacos_db'
-	});
+	port: 8889,
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'tacos_db'
+});
+};
+
 
 // Make the connection to MySQL
 connection.connect(function(err) {
